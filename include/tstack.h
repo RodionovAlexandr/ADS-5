6184dc18a5adf4a55f010c3a27e0d4f588c200c9
RodionovAlexandr <99204870+RodionovAlexandr@users.noreply.github.com>
2022-06-10 22:43:43 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,42 @@
 
 template<typename T, int size>
 class TStack {
-
+private:
+struct stek {
+	int chislo;
+	stek* next;
+};
+	stek* my;
+	stek* head=my;
+	bool check=false;
+public:
+	void Add(int x) {
+		head = new(stek);
+		head->chislo = x;
+		head->next = my;
+		my = head;
+	}
+	int Top() {
+		return head->chislo;
+	}
+	void DelTop() {
+		head = my->next;
+		delete my;
+		my = head;
+	}
+	void MakeEm() {
+			while (head != nullptr) {
+				head = my->next;
+				delete my;
+				my = head;
+			}
+	}
+	bool EmCh() {
+		check = false;
+		if (head == nullptr) {
+			check = true;
+		}
+		return check;
+	}
 };
 #endif  // INCLUDE_TSTACK_H_
